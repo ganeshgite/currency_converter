c2c68a08a74f8ff382dd1d6e110c61421652acb6
ganeshgite <giteganesh09@gmail.com>
2025-01-02 12:56:56 +0530

    changes

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,19 @@
+import { ghPages } from 'vite-plugin-gh-pages';  
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react() ],
+  base: '/currency_converter/', // Replace with your GitHub repository name
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+      },
+    },
+  },
 })
+
+
+ 
